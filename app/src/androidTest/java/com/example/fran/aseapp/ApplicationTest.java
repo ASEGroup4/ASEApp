f56eb84df569e65e653a6c43f94f94bf28113d07
Stan-Lewry <sl410@sussex.ac.uk>
2016-11-02 22:10:07 +0000

    Begain writing Instrumented Unit Tests for the app

--- app/src/androidTest/java/com/example/fran/aseapp/ApplicationTest.java
+++ app/src/androidTest/java/com/example/fran/aseapp/ApplicationTest.java
@@ -1,13 +1,37 @@
 package com.example.fran.aseapp;
 
 import android.app.Application;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    MapsActivity mapsActivity;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Before
+    public void initialize(){
+        mapsActivity = new MapsActivity();
+    }
+
+    @Test
+    public void testWifiChecker(){
+        Boolean expectedResult = true;
+        Boolean actualResult = mapsActivity.checkWifi();
+        assertEquals(actualResult, expectedResult);
     }
 }
