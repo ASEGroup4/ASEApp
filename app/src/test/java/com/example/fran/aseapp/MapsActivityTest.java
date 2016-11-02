4aabc8789806ede7700f7f466cf54ae45c5bcd27
Stan-Lewry <sl410@sussex.ac.uk>
2016-11-02 19:03:10 +0000

    Added a test to MapsActivityTest.java

--- app/src/test/java/com/example/fran/aseapp/MapsActivityTest.java
+++ app/src/test/java/com/example/fran/aseapp/MapsActivityTest.java
@@ -1,9 +1,27 @@
 package com.example.fran.aseapp;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by stan on 02/11/16.
  */
 
 public class MapsActivityTest {
+    MapsActivity mapsActivity;
 
+    @Before
+    public void initialize(){
+        mapsActivity = new MapsActivity();
+    }
+
+    @Test
+    public void checkWifiTest(){
+
+        Boolean result = mapsActivity.checkWifi();
+        Boolean expected = true;
+        assertEquals(expected, result);
+    }
 }
