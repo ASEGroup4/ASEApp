880f9fc071f1aae578bdbdc99f45dc0b8dd9d62d
Stan-Lewry <sl410@sussex.ac.uk>
2016-11-02 16:01:40 +0000

    I dont know what I am doing please

--- app/src/test/java/com/example/fran/aseapp/MapsActivityTest.java
+++ app/src/test/java/com/example/fran/aseapp/MapsActivityTest.java
@@ -1,9 +1,16 @@
 package com.example.fran.aseapp;
 
+import org.junit.Before;
+
 /**
  * Created by stan on 02/11/16.
  */
 
 public class MapsActivityTest {
+    MapsActivity
 
+    @Before
+    public void initialize(){
+
+    }
 }
