96ceb940dfb6c1346ebacea91dba873ee61a8e66
miniblin <dil20@sussex.ac.uk>
2016-11-02 14:58:13 +0000

    made vars global so accessible in functions and stuff

--- STANS BACKEND STUFF/index.php
+++ STANS BACKEND STUFF/index.php
@@ -14,7 +14,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		 );
 
 function SQLinsert($AppKey, $LocationLat, $LocationLong, $UserID, $Time){
-	$connection = new PDO("mysql:host=".$serverName.";dbname=".$dbName, $username, $password);
+	global $serverName,$username,$password,$dbName,$connection;
+	$connection = new PDO("mysql:host=$serverName;dbname=$dbName, $username, $password");
 	$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 	$access = $connection -> prepare("SELECT DB_key FROM Auth WHERE AutoID == 1");
