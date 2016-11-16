39d1aa9fd996796cfc890920c5c01646c311057c
miniblin <dil20@sussex.ac.uk>
2016-11-16 15:27:35 +0000

    Updated php to allow for posctode search

--- STANS BACKEND STUFF/heatmap.php
+++ STANS BACKEND STUFF/heatmap.php
@@ -8,6 +8,7 @@ $dbname = "locationdb";
 //location from app
 $latitude = $_REQUEST['latitude'];
 $longitude = $_REQUEST['longitude'];
+$postcode = $_REQUEST['postcode'];
 
 
 $sqlStatement;
@@ -20,14 +21,15 @@ $sqlStatement;
 function getLocalHeatMap(){
       //  $latitude = 50.839523;
       //  $longitude=-0.118259;
-		global $latitude;
-		global $longitude;
-     global $servername;
-    global $username;
-    global $password;
-    global $dbname;
-    $latitude = number_format((float)$latitude, 3, '.', '');
-    $longitude = number_format((float)$longitude, 3, '.', '');
+	global $latitude;
+	global $longitude;
+     	global $servername;
+	global $username;
+	global $password;
+	global $dbname;
+	global $postcode;
+	$latitude = number_format((float)$latitude, 3, '.', '');
+	$longitude = number_format((float)$longitude, 3, '.', '');
 	
 	$range = 0.001;
 	$minLat = ""+($latitude-$range);
@@ -37,44 +39,46 @@ function getLocalHeatMap(){
     
 	
 	$longitude =""+$longitude;
-    $latitude =""+$latitude;
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	$latitude =""+$latitude;
+    	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    	// set the PDO error mode to exception
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
-   // $sqlStatement ='SELECT Postcode,Latitude, Longitude FROM Postcodes WHERE Latitude LIKE "'.$latitude .'%" AND Longitude LIKE "'.$longitude.'%"';
-      $sqlStatement ='SELECT Postcode,Latitude, Longitude FROM Postcodes WHERE Latitude BETWEEN '.$minLat .'AND '.$maxLat.'  AND Longitude  BETWEEN '.$minLong.' AND '.$maxLong;
-   
+	// $sqlStatement ='SELECT Postcode,Latitude, Longitude FROM Postcodes WHERE Latitude LIKE "'.$latitude .'%" AND Longitude LIKE "'.$longitude.'%"';
+      	if ($postcode==NULL){
+		$sqlStatement ='SELECT Postcode,Latitude, Longitude FROM Postcodes WHERE Latitude BETWEEN '.$minLat .'AND '.$maxLat.'  AND Longitude  BETWEEN '.$minLong.' AND '.$maxLong;
+   		$result = $conn->query($sqlStatement);
+		$output=$result->fetchAll(PDO::FETCH_ASSOC);
+		$json=json_encode($output);
+		//print $json;
+
+		$json = json_decode($json, true);
+		$postcode =$json[0]['Postcode'];
+	}
+	$postcode = substr($postcode,0, 2);
 	//echo $sqlStatement;
-    $result = $conn->query($sqlStatement);
-    $output=$result->fetchAll(PDO::FETCH_ASSOC);
-    $json=json_encode($output);
-    //print $json;
-	
-	$json = json_decode($json, true);
-	$postcode =$json[0]['Postcode'];
-	 $postcode = substr($postcode,0, 2);
+    	
 
 	//echo $postcode;
 	
 	$sqlStatement ='SELECT AveragePerPostCode.PostCode,AVG(Value), Latitude, Longitude FROM AveragePerPostCode INNER JOIN Postcodes ON Postcodes.Postcode = AveragePerPostCode.postCode WHERE Year >2014 And AveragePerPostCode.PostCode LIKE "'.$postcode .'%" GROUP BY AveragePerPostCode.PostCode ';
-    $result = $conn->query($sqlStatement);
-    $output=$result->fetchAll(PDO::FETCH_ASSOC);
-    $json=json_encode($output);
+   	$result = $conn->query($sqlStatement);
+    	$output=$result->fetchAll(PDO::FETCH_ASSOC);
+    	$json=json_encode($output);
 
-    echo $json;
-    // input long lat
-    //get postcode
-    //return nearby values
-    /*
-     * $sqlStatement ='SELECT AveragePerPostCode.PostCode,AVG(Value), Latitude, Longitude FROM AveragePerPostCode INNER JOIN Postcodes ON Postcodes.Postcode = AveragePerPostCode.postCode WHERE Year >2014 And AveragePerPostCode.PostCode LIKE "BN2%" GROUP BY AveragePerPostCode.PostCode ';
-       $result = $conn->query($sqlStatement);
-       $output=$result->fetchAll(PDO::FETCH_ASSOC);
-       $json=json_encode($output);
+    	echo $json;
+    	// input long lat
+    	//get postcode
+    	//return nearby values
+    	/*
+     	* $sqlStatement ='SELECT AveragePerPostCode.PostCode,AVG(Value), Latitude, Longitude FROM AveragePerPostCode INNER JOIN Postcodes ON Postcodes.Postcode = AveragePerPostCode.postCode WHERE Year >2014 And AveragePerPostCode.PostCode LIKE "BN2%" GROUP BY AveragePerPostCode.PostCode ';
+      	$result = $conn->query($sqlStatement);
+       	$output=$result->fetchAll(PDO::FETCH_ASSOC);
+       	$json=json_encode($output);
        print $json;
-     *
-     *
-     */
+     	*
+     	*
+     	*/
 }
 
 
